16cc18e137d75760806504a050165057465db12e
AliIbrahimov <aliibrah30@gmail.com>
2022-11-11 03:12:52 +0400

    add color&&text

--- Task_2/main.js
+++ Task_2/main.js
@@ -1,6 +1,11 @@
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(element => {
     if (element.getAttribute("data-id")>5) {
+        element.classList.add("btn-danger");
         console.log(element.textContent);
-    }
+        element.innerHTML += " Data-id: " + element.getAttribute("data-id")
+        }else {
+        element.classList.add("btn-success");
+        element.innerHTML += " Data-id: " + element.getAttribute("data-id")
+        }
 });
